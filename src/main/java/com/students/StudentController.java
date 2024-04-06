4c89a5bebf8fd4a5bc871467e68389886c0d4dad
jocelin kisenga <kisengajocelin7@gmail.com>
2024-04-06 23:08:18 +0200

    add student repo

--- src/main/java/com/students/StudentController.java
+++ src/main/java/com/students/StudentController.java
@@ -9,12 +9,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.students.demo.Student;
+
 @RestController
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
 
-    	@GetMapping
-	public List<Student> hello(){
+    @GetMapping
+	public List<Student> getStudents(){
 		return java.util.List.of(
 			new Student((long) 1,
 			 "jocelin",
