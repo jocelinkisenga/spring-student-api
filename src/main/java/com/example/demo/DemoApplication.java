bda666ea28b38b67d34382ffacd252a1e42dae0a
jocelin kisenga <kisengajocelin7@gmail.com>
2024-04-07 10:37:01 +0200

    [feat] student controller

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,14 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
-
-
 
 }
--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,6 +1,21 @@
 package com.example.demo.student;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/students")
 public class StudentController {
 
+    public List<Student> getStudents() {
+        return List.of(
+                new Student((long) 1,
+                        "jocelin kisenga",
+                        "jocelin@gmail.com",
+                        LocalDate.of(2000, Month.MARCH, 25)));
+    }
 }
