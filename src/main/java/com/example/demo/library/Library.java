b750d94546f2fe11d271d4b834d98416410bdfd2
jocelin kisenga <kisengajocelin7@gmail.com>
2024-04-08 08:52:06 +0200

    [feat] library entity

--- src/main/java/com/example/demo/library/Library.java
+++ src/main/java/com/example/demo/library/Library.java
@@ -1,5 +1,64 @@
 package com.example.demo.library;
 
+
+import jakarta.persistence.*;
+
+@Entity
 public class Library {
+    @Id
+
+    @SequenceGenerator(name = "library_sequence", sequenceName = "library_sequence", allocationSize = 1)
+    @GeneratedValue(
+
+    )
+
+    private Long Id;
+    private String name;
+    private String  adress;
+    
+    public Library() {
+    }
+
+    public Library(Long id, String name, String adress) {
+        Id = id;
+        this.name = name;
+        this.adress = adress;
+    }
+
+    public Library(String name, String adress) {
+        this.name = name;
+        this.adress = adress;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    @Override
+    public String toString() {
+        return "Library [Id=" + Id + ", name=" + name + ", adress=" + adress + "]";
+    }
+
+    
 
 }
